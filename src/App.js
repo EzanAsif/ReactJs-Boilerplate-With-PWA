f3fcb5a8cd85cd448802059e4d44f7fd9fd0ea88
EzanAsif <ezanasif1972@gmail.com>
2022-06-02 10:10:33 +0500

    Setup of folder structure [DONE]

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Provider, useSelector, useDispatch } from "react-redux";
+import { store } from "./Store/index";
+import { userDataFromLocalStorage } from "./Store/Reducers/AuthReducer";
+
+const userDataFunc = async () => {
+  try {
+    let value;
+    value = await localStorage.getItem("token").then((res) => {
+      return res;
+    });
+    return value;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const UserAuthenticated = () => {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+  console.log(`this is state`);
+  console.log(state);
+  React.useEffect(() => {
+    (async () => {
+      userDataFunc().then((res) => {
+        console.log("this is res in App");
+        console.log(res);
+        let v;
+        if (res) v = JSON.parse(res);
+        if (v && v.userId) {
+          dispatch(userDataFromLocalStorage(v));
+        }
+      });
+    })().catch((err) => {
+      console.error(err);
+    });
+  }, []);
+
+  return null;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <UserAuthenticated />
+      <div className="App">
+        <p>Running</p>
+      </div>
+    </Provider>
   );
 }
 
